4006675d0062428bf6a413221d478be31f3c4163
Sheriffdeen Yusuf <sherifdeenmuhammed@outlook.com>
2022-10-05 23:43:41 +0100

    feat: :sparkles: add font configurations

---
+++ hooks/useCachedResources.ts
@@ -0,0 +1,89 @@
+import {
+  Urbanist_100Thin,
+  Urbanist_100Thin_Italic,
+  Urbanist_200ExtraLight,
+  Urbanist_200ExtraLight_Italic,
+  Urbanist_300Light,
+  Urbanist_300Light_Italic,
+  Urbanist_400Regular,
+  Urbanist_400Regular_Italic,
+  Urbanist_500Medium,
+  Urbanist_500Medium_Italic,
+  Urbanist_600SemiBold,
+  Urbanist_600SemiBold_Italic,
+  Urbanist_700Bold,
+  Urbanist_700Bold_Italic,
+  Urbanist_800ExtraBold,
+  Urbanist_800ExtraBold_Italic,
+  Urbanist_900Black,
+  Urbanist_900Black_Italic,
+  useFonts,
+} from "@expo-google-fonts/urbanist";
+//import { FontAwesome } from "@expo/vector-icons";
+//import * as Font from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import * as React from "react";
+
+export default function useCachedResources() {
+  const [isLoadingComplete, setLoadingComplete] = React.useState(false);
+
+  let [fontsLoaded] = useFonts({
+    Urbanist_700Bold,
+    Urbanist_500Medium,
+    Urbanist_100Thin,
+    Urbanist_100Thin_Italic,
+    Urbanist_200ExtraLight,
+    Urbanist_200ExtraLight_Italic,
+    Urbanist_300Light,
+    Urbanist_300Light_Italic,
+    Urbanist_400Regular,
+    Urbanist_400Regular_Italic,
+    Urbanist_500Medium_Italic,
+    Urbanist_600SemiBold,
+    Urbanist_600SemiBold_Italic,
+    Urbanist_700Bold_Italic,
+    Urbanist_800ExtraBold,
+    Urbanist_800ExtraBold_Italic,
+    Urbanist_900Black,
+    Urbanist_900Black_Italic,
+  });
+
+  // React.useEffect(() => {
+  //   // onAuthStateChanged returns an unsubscriber
+  //   const unsubscribeAuth = onAuthStateChanged(auth, async (user) => {
+  //     try {
+  //       await (user ? setAuthUser(user) : setAuthUser(null));
+  //       setLoading(false);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   });
+
+  //   // unsubscribe auth listener on unmount
+  //   return unsubscribeAuth;
+  // }, []);
+
+  // Load any resources or data that we need prior to rendering the app
+  React.useEffect(() => {
+    async function loadResourcesAndDataAsync() {
+      try {
+        SplashScreen.preventAutoHideAsync();
+
+        // Load fonts
+        //   await Font.loadAsync({
+        //     ...FontAwesome.font,
+        //   });
+      } catch (e) {
+        // We might want to provide this error information to an error reporting service
+        console.warn(e);
+      } finally {
+        setLoadingComplete(true);
+        //   SplashScreen.hideAsync();
+      }
+    }
+
+    loadResourcesAndDataAsync();
+  }, []);
+
+  return isLoadingComplete;
+}
---
+++ theme/fonts.ts
@@ -0,0 +1,37 @@
+export const config = {
+  Urbanist: {
+    100: {
+      normal: "Urbanist_100Thin",
+    },
+    200: {
+      normal: "Urbanist_200ExtraLight",
+    },
+    300: {
+      normal: "Urbanist_300Light",
+    },
+    400: {
+      normal: "Urbanist_400Regular",
+    },
+    500: {
+      normal: "Urbanist_500Medium",
+    },
+    600: {
+      normal: "Urbanist_600SemiBold",
+    },
+    700: {
+      normal: "Urbanist_700Bold",
+    },
+    800: {
+      normal: "Urbanist_800ExtraBold",
+    },
+    900: {
+      normal: "Urbanist_900Black",
+    },
+  },
+};
+
+export const fonts = {
+  heading: "Urbanist",
+  body: "Urbanist",
+  mono: "Urbanist",
+};
