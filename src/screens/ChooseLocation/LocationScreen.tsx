c5d885e652bf75a8a42e2140a61635f41868f808
Sheriffdeen Yusuf <sherifdeenmuhammed@outlook.com>
2022-11-17 23:27:40 +0100

    feat: :construction: add choose location screen ui

---
+++ src/screens/ChooseLocation/LocationScreen.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "native-base";
+import React from "react";
+
+export default function LocationScreen() {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+}
